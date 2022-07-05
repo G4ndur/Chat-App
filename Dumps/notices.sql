5439df66bf775d61e668143544de24ad9bf34d61
G4ndur <schwennbenito@gmail.com>
2022-07-05 10:17:31 +0200

    Del

--- Dumps/notices.sql
+++
@@ -1,9 +0,0 @@
-create table `notice-app`.notices
-(
-    `key`      varchar(45) not null
-        primary key,
-    payload    mediumtext  null,
-    created_at datetime    not null,
-    updated_at datetime    null
-);
-
